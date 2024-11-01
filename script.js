6fb80c165e817f76d6726d892b906d361ebc7ccf
AnGeD13 <annagetun13@gmail.com>
2024-11-01 19:24:18 +1000

    task-done

---
+++ script.js
@@ -0,0 +1,11 @@
+const bubbleBtn = document.querySelector(".js-close-bubble");
+const bubbleBlock = document.querySelector(".bubble");
+const paymentItem = document.querySelectorAll(".footer__payment-item");
+
+bubbleBtn.addEventListener("click", function () {
+  bubbleBlock.style.display = "none";
+});
+
+function preventLink(ev) {
+  ev.preventDefault();
+}
